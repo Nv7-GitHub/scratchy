057f4a883654f62ddafaf15c6a804631d90bba83
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-08-29 10:11:02 -0700

    Update bindings

--- scratch/bindings.go
+++ scratch/bindings.go
@@ -1,13 +1,25 @@
 // scratch defines bindings to builtin scratch functions
 package scratch
 
-func Say(text string)
-func SayFor(text string, time float64)
-
 func Cast[T any](v any) T {
 	return v.(T)
 }
 
+// Looks
+func Say(text string)
+func SayFor(text string, time float64)
+func Think(text string)
+func ThinkFor(text string, time float64)
+func Show()
+func Hide()
+
+// Control
 func Wait(time float64)
 
-func Clear[T any](arr []T) {}
+// Lists
+func Append[T any](arr []T, v T)          {}
+func Remove[T any](arr []T, ind int)      {}
+func Insert[T any](arr []T, ind int, v T) {}
+func Clear[T any](arr []T)                {}
+func Find[T any](arr []T, v T)            {}
+func Contains[T any](arr []T, v T) bool   { return false }
